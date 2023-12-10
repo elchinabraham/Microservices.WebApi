ceac417ff93f8d1a2492bef3a6a6f561ec4966e8
Elchin Ibrahimli <elcin.ibrahimli@ship4wd.com>
2023-12-10 18:53:00 +0400

    Building an Ocelot API Gateway

--- Product.Microservice/Program.cs
+++ Product.Microservice/Program.cs
@@ -8,6 +8,7 @@ namespace Product.Microservice
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            //
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
